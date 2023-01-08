71afb6efe9b3a1ea1a8c97b6ed0d16e1dd1fcf90
Werner, Benjamin <benjamin.werner@mfn.berlin>
2023-01-08 19:54:30 +0100

    add deploy script add first deployed frontend version add deployed frontend to async webserver

---
+++ lib/frontend-files/FrontendFiles.h
@@ -0,0 +1,13 @@
+
+struct FrontendFileInfo{
+    std::string url;
+    std::string filepath;
+    std::string mimeType;
+  
+};
+const int frontendFilesCount = 3;
+const FrontendFileInfo frontendFileInfos[3] = {
+{"/assets/logo-da9b9095.svg", "/assets_logo-da9b9095.svg", "image/svg+xml"},
+{"/favicon.ico", "/favicon.ico", "image/vnd.microsoft.icon"},
+{"/index.html", "/index.html", "text/html"}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -7,142 +7,112 @@
 #include <Wire.h>
 #include "SPIFFS.h"
 #include <Adafruit_PWMServoDriver.h>
-
-
-
+#include "FrontendFiles.h"
 
 // Setting network credentials
-const char* ssid = "ALM4OG_RPT";
-const char* password = "domn8udomn8u2domn8u3";
+const char *ssid = "ALM4OG_RPT";
+const char *password = "domn8udomn8u2domn8u3";
 
-// Servorboard 
+// Servorboard
 // called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver board1 = Adafruit_PWMServoDriver(0x40);
 
-// Depending on your servo make, the pulse width min and max may vary, you 
+// Depending on your servo make, the pulse width min and max may vary, you
 // want these to be as small/large as possible without hitting the hard stop
 // for max range. You'll have to tweak them as necessary to match the servos you
 // have!
 // Watch video V1 to understand the two lines below: http://youtu.be/y8X9X10Tn1k
-#define SERVOMIN  125 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
+#define SERVOMIN 125 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX 600 // this is the 'maximum' pulse length count (out of 4096)
 
 // Webinterface
 
-const char* input_parameter1 = "output";
-const char* input_parameter2 = "state";
+const char *input_parameter1 = "output";
+const char *input_parameter2 = "state";
 
 const int LED1 = 4;
 const int LED2 = 16;
 const int LED3 = 17;
-// Creating a AsyncWebServer object 
+// Creating a AsyncWebServer object
 AsyncWebServer server(80);
 
-
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
-<head>
-  <title>ESPLED1 WEB SERVER</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="data:,">
-  <style>
-    html {font-family: Arial; display: inline-block; text-align: center;}
-    p {font-size: 3.0rem;}
-    body {max-width: 600px; margin:0px auto; padding-bottom: LED2px;}
-    .switch {position: relative; display: inline-block; width: 120px; height: 68px} 
-    .switch input {display: none}
-    .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; border-radius: 6px}
-    .slider:before {position: absolute; content: ""; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 3px}
-    input:checked+.slider {background-color: #b30000}
-    input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
-  </style>
-</head>
-<body>
-  <h2>ESPLED1 WEB SERVER</h2>
-  %BUTTONPLACEHOLDER%
-<script>function toggleCheckbox(element) {
-  var xhr = new XMLHttpRequest();
-  if(element.checked){ xhr.open("GET", "/update?output="+element.id+"&state=1", true); }
-  else { xhr.open("GET", "/update?output="+element.id+"&state=0", true); }
-  xhr.send();
-}
-</script>
-</body>
-</html>
-)rawliteral";
-
-String outputState(int output){
-  if(digitalRead(output)){
-    return "checked";
-  }
-  else {
-    return "";
-  }
+String outputState(int output)
+{
+    if (digitalRead(output))
+    {
+        return "checked";
+    }
+    else
+    {
+        return "";
+    }
 }
 
 // Replaces placeholder with button section in your web page
-String processor(const String& var){
-  //Serial.println(var);
-  if(var == "BUTTONPLACEHOLDER"){
-    String buttons = "";
-    buttons += "<h4>Output - GPIO LED1</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"" + String(LED1) + "\" " + outputState(LED1) + "><span class=\"slider\"></span></label>";
 
-    buttons += "<h4>Output - GPIO LED2</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\""+ String(LED2) + "\" " + outputState(LED2) + "><span class=\"slider\"></span></label>";
+void setup()
+{
+    // Serial port for debugging purposes
+    Serial.begin(115200);
 
-    buttons += "<h4>Output - GPIO LED3</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\""+ String(LED3) + "\" " + outputState(LED3) + "><span class=\"slider\"></span></label>";
+    pinMode(LED1, OUTPUT);
+    digitalWrite(LED1, LOW);
+    pinMode(LED2, OUTPUT);
+    digitalWrite(LED2, LOW);
+    pinMode(LED3, OUTPUT);
+    digitalWrite(LED3, LOW);
+    pinMode(13, OUTPUT);
+    digitalWrite(13, LOW);
 
-   buttons += "<h4>Output - GPIO 13</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"13\" " + outputState(13) + "><span class=\"slider\"></span></label>";
+    // Connect to Wi-Fi
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(1000);
+        Serial.println("Connecting to WiFi");
+    }
 
-    return buttons;
-  }
-  return String();
-}
+    // Print ESP Local IP Address
+    Serial.println(WiFi.localIP());
 
+    // Initialize SPIFFS
+    if (!SPIFFS.begin())
+    {
+        Serial.println("An Error has occurred while mounting SPIFFS");
+        return;
+    }
+    // iterate of frontendFiles
+    for (int i = 0; i < frontendFilesCount; i++)
+    {
+        // open file for writing
+        Serial.println("set route on ");
+        Serial.println(frontendFileInfos[i].url.c_str());
+        Serial.println(frontendFileInfos[i].filepath.c_str());
+        Serial.println(frontendFileInfos[i].mimeType.c_str());
+        const FrontendFileInfo fileInfo = frontendFileInfos[i];
+        server.on(frontendFileInfos[i].url.c_str(), HTTP_GET, [fileInfo](AsyncWebServerRequest *request)
+                  {
+            Serial.println("get /");
+            request->send(SPIFFS, fileInfo.filepath.c_str(), fileInfo.mimeType.c_str(), false); });
+    }
 
-
-void setup(){
-  // Serial port for debugging purposes
-  Serial.begin(115200);
-
-pinMode(LED1,OUTPUT);
-digitalWrite(LED1, LOW);
-pinMode(LED2, OUTPUT);
-digitalWrite(LED2, LOW);
-pinMode(LED3, OUTPUT);
-digitalWrite(LED3, LOW);
-pinMode(13, OUTPUT);
-digitalWrite(13, LOW);
-
-  
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi");
-  }
-
-  // Print ESP Local IP Address
-  Serial.println(WiFi.localIP());
-
-   // Initialize SPIFFS
-  if(!SPIFFS.begin()){
-    Serial.println("An Error has occurred while mounting SPIFFS");
-    return;
-  }
-
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    // Route for root / web page
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
     Serial.println("get /");
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });
-  server.on("/favicon.ico", HTTP_GET, [](AsyncWebServerRequest *request){
-    Serial.println("get /favicon.ico");
-    request->send(SPIFFS, "/favicon.ico", String(), false, processor);
-  });
+    request->send(SPIFFS, "/index.html", "text/html", false); });
+    //   server.on("/favicon.ico", HTTP_GET, [](AsyncWebServerRequest *request){
+    //     Serial.println("get /favicon.ico");
+    //     request->send(SPIFFS, "/favicon.ico", "image/vnd.microsoft.icon", false);
+    //   });
+    //   server.on("/assets/logo-da9b9095.svg", HTTP_GET, [](AsyncWebServerRequest *request){
+    //     Serial.println("get /favicon.ico");
+    //     request->send(SPIFFS, "/assets_logo-da9b9095.svg", "image/svg+xml", false);
+    //   });
 
-
-  // Send a GET request to <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
-  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    // Send a GET request to <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
+    server.on("/update", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
     String inputMessage1;
     String inputMessage2;
     // GET input1 value on <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
@@ -159,17 +129,16 @@ digitalWrite(13, LOW);
     Serial.print(inputMessage1);
     Serial.print(" - Set to: ");
     Serial.println(inputMessage2);
-    request->send(200, "text/plain", "OK");
-  });
+    request->send(200, "text/plain", "OK"); });
 
-  // Start server
-  server.begin();
+    // Start server
+    server.begin();
 
-  // Servo Stuff
-  Serial.println("32 channel Servo test!");
+    // Servo Stuff
+    Serial.println("32 channel Servo test!");
 
-  board1.begin();
-  board1.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+    board1.begin();
+    board1.setPWMFreq(60); // Analog servos run at ~60 Hz updates
 }
 
 /*
@@ -178,25 +147,27 @@ digitalWrite(13, LOW);
  * also prints the value on seial monitor
  * written by Ahmad Nejrabi for Robojax, Robojax.com
  */
-int angleToPulse(int ang){
-   int pulse = map(ang,0, 180, SERVOMIN,SERVOMAX);// map angle of 0 to 180 to Servo min and Servo max 
-   Serial.print("Angle: ");Serial.print(ang);
-   Serial.print(" pulse: ");Serial.println(pulse);
-   return pulse;
+int angleToPulse(int ang)
+{
+    int pulse = map(ang, 0, 180, SERVOMIN, SERVOMAX); // map angle of 0 to 180 to Servo min and Servo max
+    Serial.print("Angle: ");
+    Serial.print(ang);
+    Serial.print(" pulse: ");
+    Serial.println(pulse);
+    return pulse;
 }
 
-void loop() {
-
+void loop()
+{
 
     // for( int angle =0; angle<300; angle +=1){
     //   for(int i=0; i<16; i++)
-    //     {      
+    //     {
     //         board1.setPWM(i, 0, angleToPulse(angle) );
     //     }
     //   delay(10);
     // }
-  
-// robojax PCA9865 16 channel Servo control
-  delay(500);
 
+    // robojax PCA9865 16 channel Servo control
+    delay(500);
 }
