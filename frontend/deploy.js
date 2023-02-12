bf8f18be775af31fb085d77819155af3a8803514
Werner, Benjamin <benjamin.werner@mfn.berlin>
2023-02-12 17:17:55 +0100

    working again deploy script add curren version

---
+++ frontend/deploy.js
@@ -0,0 +1,74 @@
+const fs = require("fs");
+const path = require("path");
+const { existsSync } = require("fs");
+const { lookup } = require("mime-types");
+
+const buildFolder = "dist";
+const dataFolder = "../data";
+const libFolder = "../lib/frontend-files";
+// read build folder recursively and get all files
+function walk(dir) {
+  const entries = fs.readdirSync(dir);
+  let ret = [];
+  for (const entry of entries) {
+    const fullPath = path.join(dir, entry);
+    const info = fs.statSync(fullPath);
+    if (info.isDirectory()) {
+      ret = [...ret, ...walk(fullPath)];
+    } else {
+      ret = [...ret, fullPath];
+    }
+  }
+
+  return ret.map((file) => file.replace(buildFolder, ""));
+}
+
+(function () {
+  // get all files in build folder
+  const files = walk(buildFolder);
+  const fileInfos = files.map((file) => ({
+    fileUrl: file,
+    filepath: file.replace(/\//g, "_").replace(/^_/, "/"),
+    mimeType: lookup(file),
+  }));
+
+  console.log(fileInfos);
+  // empty data folder
+  if (existsSync(dataFolder))
+    fs.rmSync(dataFolder, { recursive: true, force: true });
+  // create data
+  fs.mkdirSync(dataFolder, { recursive: true });
+
+  // copy files to data folder
+  for (const fileInfo of fileInfos) {
+    fs.copyFileSync(
+      path.join(buildFolder, fileInfo.fileUrl),
+      path.join(dataFolder, fileInfo.filepath)
+    );
+  }
+  // check if lib folder exists
+  if (existsSync(libFolder))
+    fs.rmSync(libFolder, { recursive: true, force: true });
+  // create data
+  fs.mkdirSync(libFolder, { recursive: true });
+
+  // create c++ struct array for files
+
+  const fileStructs = fileInfos.map(
+    (fileInfo) =>
+      `{"${fileInfo.fileUrl}", "${fileInfo.filepath}", "${fileInfo.mimeType}"}`
+  );
+  const fileStructsStr = fileStructs.join(",\n");
+  const fileStructsArray = `
+struct FrontendFileInfo{
+    std::string url;
+    std::string filepath;
+    std::string mimeType;
+  
+};
+const int frontendFilesCount = ${fileStructs.length};
+const FrontendFileInfo frontendFileInfos[${fileStructs.length}] = {\n${fileStructsStr}\n};`;
+  // write c++ struct array to file
+
+  fs.writeFileSync(path.join(libFolder, "FrontendFiles.h"), fileStructsArray);
+})();
--- lib/frontend-files/FrontendFiles.h
+++ lib/frontend-files/FrontendFiles.h
@@ -5,9 +5,26 @@ struct FrontendFileInfo{
     std::string mimeType;
   
 };
-const int frontendFilesCount = 3;
-const FrontendFileInfo frontendFileInfos[3] = {
-{"/assets/logo-da9b9095.svg", "/assets_logo-da9b9095.svg", "image/svg+xml"},
+const int frontendFilesCount = 20;
+const FrontendFileInfo frontendFileInfos[20] = {
+{"/assets/About.fe89273a.js", "/assets_About.fe89273a.js", "application/javascript"},
+{"/assets/Default.a826eee3.js", "/assets_Default.a826eee3.js", "application/javascript"},
+{"/assets/Default.dc371d9b.css", "/assets_Default.dc371d9b.css", "text/css"},
+{"/assets/Home.da1d4e81.js", "/assets_Home.da1d4e81.js", "application/javascript"},
+{"/assets/Leds.2977a807.js", "/assets_Leds.2977a807.js", "application/javascript"},
+{"/assets/Leds.dc98024d.css", "/assets_Leds.dc98024d.css", "text/css"},
+{"/assets/VAvatar.4e5daafb.js", "/assets_VAvatar.4e5daafb.js", "application/javascript"},
+{"/assets/VAvatar.ab88a885.css", "/assets_VAvatar.ab88a885.css", "text/css"},
+{"/assets/VBtn.53827581.js", "/assets_VBtn.53827581.js", "application/javascript"},
+{"/assets/VBtn.f134a7f0.css", "/assets_VBtn.f134a7f0.css", "text/css"},
+{"/assets/VRow.37f80755.css", "/assets_VRow.37f80755.css", "text/css"},
+{"/assets/VRow.9f189ef1.js", "/assets_VRow.9f189ef1.js", "application/javascript"},
+{"/assets/index.15a97bb8.js", "/assets_index.15a97bb8.js", "application/javascript"},
+{"/assets/index.7e875e7a.css", "/assets_index.7e875e7a.css", "text/css"},
+{"/assets/logo.3f834fa8.svg", "/assets_logo.3f834fa8.svg", "image/svg+xml"},
+{"/assets/router.26356f0a.css", "/assets_router.26356f0a.css", "text/css"},
+{"/assets/router.6fe14ff4.js", "/assets_router.6fe14ff4.js", "application/javascript"},
+{"/assets/webfontloader.b777d690.js", "/assets_webfontloader.b777d690.js", "application/javascript"},
 {"/favicon.ico", "/favicon.ico", "image/vnd.microsoft.icon"},
 {"/index.html", "/index.html", "text/html"}
 };
